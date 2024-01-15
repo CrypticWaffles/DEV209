cb1bebe18f89bd621130bcb66592abc2448e7172
Miles <90667845+CrypticWaffles@users.noreply.github.com>
2024-01-15 14:03:47 -0800

    Update main.js
    full js for creating the normal & reversed arrays and adding them to an unordered list on the webpage

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-var words = [];
+// Create array & prompt user for entries
+var wordArr = [];
 for (var i = 0; i < 3; i++){
-  words.push(prompt("Enter a word please,"));
+  wordArr.push(prompt("Enter a word please,"));
 }
-alert(words);
+
+addToList("list1", wordArr);
+
+// reverse words and add to second list
+var newWordArr = [];
+for (var t = 0; t < wordArr.length; t++){
+  newWordArr.push(reverseString(wordArr[t]));
+}
+
+addToList("list2", newWordArr);
+
+// Functions
+function addToList(l, a){
+  /* Function to add array items to a list
+     inputs: list ID & array */
+  for(var j = 0; j < a.length; j++){
+    var entry = document.createElement("LI");
+    var word = document.createTextNode(a[j]);
+    entry.append(word);
+    document.getElementById(l).appendChild(entry);
+  }
+}
+
+function reverseString(str) {
+  /* Function reverse a string by splitting it into an array, 
+     reverseing the array, and rejoining the array */
+  var splitWord = str.split("");
+  var revWord = splitWord.reverse();
+  var newWord = revWord.join("");
+  return newWord;
+}
